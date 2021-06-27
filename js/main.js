279adb3e5772a4cb6416ced5caaf47a84bcb8fbc
darlianispineda <darlisara@gmail.com>
2021-06-27 19:57:06 -0400

    cambios varios

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
-﻿const nav= document.getElementById("nav");
+﻿const barra_nav= document.getElementById("nav");
 
 window.addEventListener("scroll",()=>{//evento scroll
     let actual= window.pageYOffset;// Obtengo la posición en Y actual del documento
 
     if (actual>=100){//A partir de esta altura el menú deja de ser transparente
-        nav.classList.remove("nav1");
-        nav.classList.add("nav2");
+        barra_nav.classList.remove("nav1");
+        barra_nav.classList.add("nav2");
     }else{
-        nav.classList.add("nav1");
-        nav.classList.remove("nav2");
+        barra_nav.classList.add("nav1");
+        barra_nav.classList.remove("nav2");
     }
     
 })
+
 
 
 
